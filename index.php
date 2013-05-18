57a4437e1cc85509b8a6b4ac68771be2423822e7
Joseph Finlaysonlinux <joseph.finlayson@gmail.com>
2013-05-18 19:03:30 +0100

    Creating gradients

--- index.php
+++ index.php
@@ -16,34 +16,94 @@
 		* { padding: 0; margin: 0;  }
 		body { background-color: #333; }
 		#header{
-			background-color: white;
-			opacity: .4;
+			background-color: black;
+			opacity: .3;
 			position: absolute;
-			width: 100%;
 			display: inline;
 			top:0px; margin:auto; 
 			font-style: bold;
-			padding: 3px;
-			height: 2em;
+			padding: 0px;
+			overflow: hidden;
+			min-height:30px !important;
 			border-radius: 0px 0px 10px 10px;
 			z-index: 0;
 			top:0px; }
-		
+		#quote{
+		text-align:right;
+		font-variant: small-caps;
+		font-style: italic;
+		margin-top: -10px;
+		margin: 0 5px 0 0;}
+
 		p.ontop{
 			font-size: 1.5em;
 			font-style: italic;
 			color: black;
 			text-align: center;
+			margin-bottom: auto; 
 			opacity: 1;
-			top:0.2em;
+			/*top:0.2em; */
 			position: relative;
+			color: white;
 			z-index: 1000000;}
 
-		p { color: #fff; font-family: Helvetica; font-size: 16px; line-height: 24px; margin-bottom: 22px; -webkit-font-smoothing: antialiased; }
+		p { color: #fff; font-family: "Ubuntu", sans-serif; font-weight: regular; font-size: 16px; line-height: 24px; margin-bottom: 21px; -webkit-font-smoothing: antialiased; }
 		a { color: #fff; font-weight: bold; text-decoration: none; border-bottom: 1px solid rgba(255,255,255,0.3); }
 		a:hover { color: #990000; border-bottom: 1px solid rgba(255,255,255,1); }
-		#monogram { width: 50px; padding: 50px 0 0 0; margin: 0 auto 0 50px; display: none; }
-		#about { width: 360px; float:left; padding: 0 0px 0 0px; margin: 0px 50px 0 auto !important; position: ; display: relative; clear: both; }
+		#monogram { width: 50px; padding: 0px 0 0 0; margin: 0 auto 0 50px; display: none; }
+		
+		#visibility{
+							background-color: 
+							/* rgba(233,93,34,0.5); */ 	/* orange, ‘scarf’ colour */
+							/* rgba(1,120,144,0.4);	*/	/* blue/cyan */
+							/* rgba(70,90,170,0.38); */		/* more blue/violet */
+							 rgba(1, 1, 1, 0.38) ;		/* another one */
+							/* rgba(160,38,0,0.52); 	*/		/* new take on orange */
+							/* rgba(35, 177, 0, 0.47);	*/		/* good green*/
+							/* rgba(54, 201, 18, 0.54);	*/			/* good green II */
+							/* rgba(111,14,146,0.3); */ 	/* violet */
+							/* rgba(71,4,95,0.3);*/  	/* violet II */
+							/* rgba(90,35,110,0.3); */  	/* violet III */						
+							  /* rgba(10,171,29,0.5);  */	/* green */
+							/* textblock background colour, isolated */ 
+							/* -moz-border-radius: 10px; -webkit-border-radius: 10px; -khtml-border-radius: 10px; border-radius: 10px; */
+							 -webkit-border-top-right-radius: 25px;
+							-webkit-border-bottom-right-radius: 25px;
+							-moz-border-radius-topright: 25px;
+							-moz-border-radius-bottomright: 25px;
+							border-top-right-radius: 25px;
+							border-bottom-right-radius: 25px; 
+							width: 350px; 
+							padding: 20px 12px 1px 35px;  
+							margin: 75px auto 20 5px; 
+							border: dotted 1px;
+
+							background: 
+							linear-gradient(45deg, rgba(1,1,1,0.38) 45px, transparent 45px)64px 64px,
+linear-gradient(45deg, rgba(1,1,1,0.38) 45px, transparent 45px,transparent 91px, rgba(1,12,1,0.38) 91px, rgba(1,12,1,0.38) 135px, transparent 135px),
+linear-gradient(-45deg, rgba(1,1,1,0.38) 23px, transparent 23px, transparent 68px, rgba(1,1,1,0.38) 68px,rgba(1,1,1,0.38) 113px,transparent 113px,transparent 158px,rgba(1,1,1,0.38)158px);
+background-color:rgba(1,12,1,0.38);
+background-size: 128px 128px;
+							/* position:absolute; bottom:0;
+							margin: auto auto 190px 0px; */
+		}
+
+
+
+		#about 
+		{ width: 360px; 
+		/*float:left; */ 
+		/*padding: 50px 0 0 50px; 
+		background-color: white;
+		opacity: 1.0
+		border-radius: 3px
+		margin: 0px 50px 0 
+		auto !important; 
+		position: ; 
+		display: relative; 
+		clear: both; */ 
+	width: 350px; padding: 0 0 0 0; opacity: 0.9; }
+
 		/*@media ( #monogram { position: relative; width: auto; padding: 20px; margin: 0; }
 		#about { position: relative; width: auto; padding: 20px; margin: 0 !important; }*/
 		background-image { opacity: 0.2; }
@@ -52,24 +112,29 @@
 </head>	
     
 <body>
-	<p class = "ontop"> "All mimsy were the borogoves,
-And the mome raths outgrabe" </p>
-<div id = "header">
-</div>
+
+<p class = "ontop"> "All mimsy were the borogoves,
+And the mome raths outgrabe" 
+	</p>
+<div id = "header"></div>
+<div id ="quote"> <p> Jabberwocky - Lewis Carroll </p> </div>
 <div style="clear:both;"> </div>
+
+
 <!-- swap out for a logo -->
 <!-- <div id="monogram">
 	
 </div>  -->
 
-<!-- swap out for information about yourself -->
 
+<!-- swap out for information about yourself -->
+<div id="visibility">
 <div id="about">
 	<p>Joe is a cofounder. A cofounder of what he isn't sure yet. But it's going to happen soon. He's currently working at  <a href="http://accenture.com">Accenture</a>, a global management consultancy - it's not completely soul-sucking. Aside from client work, side-projects include an amazon-bitcoin interface and a way for twitter users to meet each other fo' reals.</p>
-	<p>He writes about tech, politics and stuff that he stole from facebook notes at his <a href="http://blog.mimsy.co">blog</a>. Listens to all his music on <a href="http://www.last.fm/users/jungboho/">last.fm</a> and asks the occasional question on <a href="http://www.quora.com/Joe-Finlayson">Quora</a>. 
+	<p>He writes about tech, politics and stuff that he stole from facebook notes at his <a href="http://blog.mimsy.co">blog</a>. Listens to all his music on <a href="http://www.last.fm/users/jungboho/">last.fm</a> and asks the occasional question on <a href="http://www.quora.com/Joe-Finlayson">Quora</a>. </p>
 	<p>This website is absurdly hosted on <a href="http://heroku.com">Heroku</a> because I'm cheap.</p> 
-	<p>If you like you can <a href=	"http://uk.linkedin.com/pub/joseph-finlayson/53/7a3/484/">hire</a> me. I'm available for web/tech consultancy and design. Send me an email at <a  href="mailto:joe@mimsy.co">joe@mimsy.co</a>.
-
+	<p>If you like you can <a href=	"http://uk.linkedin.com/pub/joseph-finlayson/53/7a3/484/">hire</a> me. I'm available for web/tech consultancy and design. Send me an email at <a  href="mailto:joe@mimsy.co">joe@mimsy.co</a>.</p>
+</div>
 </div>
 
 <!--include jquery & backstretch-->
